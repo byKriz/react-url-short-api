172e01b65eeeac8f58f574b3167896a9624aedbd
bykriz <cristianesquivelh@hotmail.com>
2023-03-23 16:26:13 -0400

    agregando alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@icons": path.resolve(__dirname, "./src/icons"),
+      "@images": path.resolve(__dirname, "./src/images"),
+    },
+  },
+});
