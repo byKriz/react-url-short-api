957d5e2fb53b8d51f891c05f1bf5e4e51e5cc913
bykriz <cristianesquivelh@hotmail.com>
2023-03-26 22:36:58 -0400

    agregando la logica del mensaje de error

--- src/containers/LinkShorter/LinkShorter.jsx
+++ src/containers/LinkShorter/LinkShorter.jsx
@@ -5,28 +5,32 @@ import axios from "axios";
 
 export const LinkShorter = () => {
   const [url, setUrl] = useState("");
-  const [shortUrl, setShortUrl] = useState("");
+  const [emptyInput, setEmptyInput] = useState(false);
   const [urls, setUrls] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await axios.post(
-        `https://api.shrtco.de/v2/shorten?url=${url}`
-      );
-      //   setShortUrl(response.data.result.short_link);
-      setUrls([
-        ...urls,
-        {
-          original: url,
-          short: response.data.result.short_link,
-          full_short: response.data.result.full_short_link,
-        },
-      ]);
-      setUrl("");
-    } catch (e) {
-      console.error(e);
+    if (url !== "") {
+      try {
+        const response = await axios.post(
+          `https://api.shrtco.de/v2/shorten?url=${url}`
+        );
+        setUrls([
+          ...urls,
+          {
+            original: url,
+            short: response.data.result.short_link,
+            full_short: response.data.result.full_short_link,
+          },
+        ]);
+        setUrl("");
+        setEmptyInput(false);
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      setEmptyInput(true);
     }
   };
 
@@ -42,13 +46,16 @@ export const LinkShorter = () => {
           />
           <button type="submit">Shorten It!</button>
         </form>
-        {/* {shortUrl && <a href={shortUrl}>{shortUrl}</a>} */}
+        {emptyInput === true && <p>Please add link</p>}
       </div>
       <div className={styles.linksContainer}>
         <ul>
           {urls.map((url, i) => (
             <li key={i}>
-              <a href={url.full_short}>{url.short}</a> - {url.original}
+              <a href={url.full_short} target="_blank">
+                {url.short}
+              </a>{" "}
+              - {url.original}
             </li>
           ))}
         </ul>
