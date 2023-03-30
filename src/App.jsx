e19e6d797379a01df820536986f218f4fcc8f672
bykriz <cristianesquivelh@hotmail.com>
2023-03-30 16:56:10 -0400

    creando la seccion de detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header/Header'
+import { Details } from './containers/Details/Details'
 import { Hero } from './containers/Hero/Hero'
 import { LinkShorter } from './containers/LinkShorter/LinkShorter'
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <LinkShorter />
+      <Details />
 
     </div>
   )
---
+++ src/containers/Details/Details.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Details.module.scss";
+
+export const Details = () => {
+  return (
+    <section className={styles.Details}>
+      <div className={styles.titleContainer}>
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the with our advanced
+          statistic dashboard
+        </p>
+      </div>
+      <div className={styles.features}>
+        
+      </div>
+    </section>
+  );
+};
